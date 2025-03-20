44928711ce0ad6218c0ba0bedb13b815e511ccf5
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2025-03-20 16:29:24 -0500

    adds handler files needed for initial project, adds firebase application and enables cors to main application

--- backend/src/helpers/createApp/index.ts
+++ backend/src/helpers/createApp/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 
 export function createApp () {
+    // Create express application
     const app = express();
+
+    // Routes go below
 
     return app;
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,23 @@
 import { createApp } from './helpers/createApp';
+import cors from 'cors';
+import 'dotenv/config';
 
+// Create express application
 const app = createApp();
 
-const PORT = 3000;
+// Enable application to use cors
+app.use(cors());
 
+// Create and setup firebase application
+require('dotenv').config();
+var admin = require('firebase-admin');
+var serviceAccount = require('../../../firebase-service-account.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+// Start app on specified port
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
