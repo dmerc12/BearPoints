cc4e2e4f22a551df49f69b53981894a798c442bf
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2025-06-20 13:13:36 -0500

    added rest config exposing entity id's and cors config

---
+++ api/src/main/java/com/bearpoints/api/config/RestConfig.java
@@ -0,0 +1,36 @@
+package com.bearpoints.api.config;
+
+import com.bearpoints.api.domain.*;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class RestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(
+            RepositoryRestConfiguration config,
+            CorsRegistry cors) {
+        config.exposeIdsFor(
+                User.class, Teacher.class, Student.class, BehaviorType.class,
+                BragLog.class, RewardItem.class, StudentReward.class
+        );
+        config.setDefaultPageSize(Integer.MAX_VALUE);
+        config.setMaxPageSize(Integer.MAX_VALUE);
+        cors.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name()
+                )
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
