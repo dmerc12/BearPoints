72d17011e8b7c0239bd73bbed98278e0b32ca5a6
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2025-07-05 21:02:55 -0500

    added documentation and unit test cases for FirebaseUserDetails

--- api/src/main/java/com/bearpoints/api/security/FirebaseUserDetails.java
+++ api/src/main/java/com/bearpoints/api/security/FirebaseUserDetails.java
@@ -9,14 +9,27 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.Collections;
 
+/**
+ * Represents logged-in user.
+ * <p>Extends spring security user details
+ *
+ * @author Dylan Mercer
+ * @version 1.0
+ */
 @Getter
 public class FirebaseUserDetails implements UserDetails {
     private final User user;
 
+    /**
+     * Instantiates a new Firebase user details.
+     *
+     * @param user the user
+     */
     public FirebaseUserDetails(User user) {
         this.user = user;
     }
 
+    /** Get user's role */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return Collections.singletonList(
@@ -24,31 +37,37 @@ public class FirebaseUserDetails implements UserDetails {
         );
     }
 
+    /** OAuth used in place of passwords */
     @Override
     public String getPassword() {
         return null;
     }
 
+    /** Email used in place of username */
     @Override
     public String getUsername() {
         return user.getEmail();
     }
 
+    /** Accounts do not expire */
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    /** Accounts do not lock */
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    /** Credentials do not expire */
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+    /** User is always enabled */
     @Override
     public boolean isEnabled() {
         return true;
