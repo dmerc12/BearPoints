7acfdee914c73a3b9155bcdcf6a3979c47f96099
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2025-07-07 16:08:13 -0500

    added documentation for RunnableThrowing

--- api/src/main/java/com/bearpoints/api/exception/RunnableThrowing.java
+++ api/src/main/java/com/bearpoints/api/exception/RunnableThrowing.java
@@ -2,7 +2,21 @@ package com.bearpoints.api.exception;
 
 import java.io.IOException;
 
+/**
+ * Functional interface representing a runnable operation that can throw an IOException.
+ * <p>Similar to {@link java.lang.Runnable} but allows checked IOExceptions to be thrown.
+ * Used primarily for operations involving I/O like Google Sheets API calls.
+ *
+ * @see java.lang.Runnable
+ * @version 1.0
+ * @author Dylan Mercer
+ */
 @FunctionalInterface
 public interface RunnableThrowing {
+    /**
+     * Executes the operation, potentially throwing an IOException.
+     *
+     * @throws IOException if an I/O error occurs during execution
+     */
     void run() throws IOException;
 }
