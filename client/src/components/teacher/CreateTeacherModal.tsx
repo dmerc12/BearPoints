0108f17132ae1c5e53b4cb1fca4037ae11ce53b4
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2025-08-30 09:51:49 -0500

    adds TeacherForm to use in create/edit teacher modals, integrates into CreateTeacherModal

--- client/src/components/teacher/CreateTeacherModal.tsx
+++ client/src/components/teacher/CreateTeacherModal.tsx
@@ -1,9 +1,9 @@
 import { Teacher, Role, GradeLevel } from '../../services/types';
 import { addTeacher } from '../../store/slices/teachersSlice';
 import { useTeacherForm } from '../../hooks/useTeacherForm';
-import { Form, Row, Col, Alert } from 'react-bootstrap';
-import { formatGrade } from '../../utils/formatGrades';
 import { useAppDispatch } from '../../store/hooks';
+import { TeacherForm } from './TeacherForm';
+import { Alert } from 'react-bootstrap';
 import BaseModal from '../BaseModal';
 
 interface CreateTeacherModalProps {
@@ -58,79 +58,13 @@ export function CreateTeacherModal({ show, onCancel, onSuccess }: CreateTeacherM
             disableConfirm={loading}
         >
             {error && <Alert variant='danger'>{error}</Alert>}
-            <Form>
-                <Row>
-                    <Col md={6}>
-                        <Form.Group className='mb-3'>
-                            <Form.Label>First Name</Form.Label>
-                            <Form.Control
-                                type='text'
-                                name='firstName'
-                                value={formData.firstName}
-                                onChange={handleInputChange}
-                                isInvalid={!!formErrors.firstName}
-                                disabled={loading}
-                            />
-                            <Form.Control.Feedback type='invalid'>
-                                {formErrors.firstName}
-                            </Form.Control.Feedback>
-                        </Form.Group>
-                    </Col>
-                    <Col md={6}>
-                        <Form.Group className='mb-3'>
-                            <Form.Label>Last Name</Form.Label>
-                            <Form.Control
-                                type='text'
-                                name='lastName'
-                                value={formData.lastName}
-                                onChange={handleInputChange}
-                                isInvalid={!!formErrors.lastName}
-                                disabled={loading}
-                            />
-                            <Form.Control.Feedback type='invalid'>
-                                {formErrors.lastName}
-                            </Form.Control.Feedback>
-                        </Form.Group>
-                    </Col>
-                </Row>
-                <Form.Group className='mb-3'>
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control
-                        type='email'
-                        name='email'
-                        value={formData.email}
-                        onChange={handleInputChange}
-                        isInvalid={!!formErrors.email}
-                        disabled={loading}
-                    />
-                    <Form.Text className='text-muted'>
-                        Must be an @okcps.org email address
-                    </Form.Text>
-                    <Form.Control.Feedback type='invalid'>
-                        {formErrors.email}
-                    </Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group className='mb-3'>
-                    <Form.Label>Grade</Form.Label>
-                    <Form.Select
-                        name='grade'
-                        value={formData.grade || ''}
-                        onChange={handleSelectChange}
-                        isInvalid={!!formErrors.grade}
-                        disabled={loading}
-                    >
-                        <option value=''>Select a grade</option>
-                        {Object.values(GradeLevel).map((grade) => (
-                            <option key={grade} value={grade}>
-                                {formatGrade(grade)}
-                            </option>
-                        ))}
-                    </Form.Select>
-                    <Form.Control.Feedback type='invalid'>
-                        {formErrors.grade}
-                    </Form.Control.Feedback>
-                </Form.Group>
-            </Form>
+            <TeacherForm 
+                formData={formData}
+                formErrors={formErrors}
+                loading={loading}
+                onInputChange={handleInputChange}
+                onSelectChange={handleSelectChange}
+            />
         </BaseModal>
     );
 }
