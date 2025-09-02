0ab51375c787021141bd73accf472086f1a312b5
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2025-09-01 23:13:18 -0500

    adds CreateAdminModal component

---
+++ client/src/components/admins/CreateAdminModal.tsx
@@ -0,0 +1,64 @@
+import { addAdmin } from '../../store/slices/adminsSlice';
+import { useAdminForm } from '../../hooks/useAdminForm';
+import { useAppDispatch } from '../../store/hooks';
+import { Role } from '../../services/types';
+import { AdminForm } from './AdminForm';
+import { Alert } from 'react-bootstrap';
+import BaseModal from '../BaseModal';
+
+interface CreateAdminModalProps {
+    show: boolean;
+    onCancel: () => void;
+    onSuccess: () => void;
+}
+
+export function CreateAdminModal({ show, onCancel, onSuccess }: CreateAdminModalProps) {
+    const dispatch = useAppDispatch();
+    
+    const { formData, formErrors, error, loading, handleInputChange, validateForm, resetForm } = useAdminForm({ show });
+    
+    const handleSubmit = () => {
+        if (!validateForm()) return;
+        const adminData = {
+            firstName: formData.firstName,
+            lastName: formData.lastName,
+            email: formData.email,
+            role: Role.ADMIN
+        };
+        dispatch(addAdmin(adminData))
+            .unwrap()
+            .then(() => {
+                onSuccess();
+                resetForm();
+            })
+            .catch((error: Error) => {
+                console.log('Failed to create admiin:', error);
+            });
+    };
+    
+    const handleClose = () => {
+        resetForm();
+        onCancel();
+    };
+    
+    return (
+        <BaseModal
+            title='Create Admin'
+            show={show}
+            onConfirm={handleSubmit}
+            onCancel={handleClose}
+            confirmText='Create'
+            cancelText='Cancel'
+            isLoading={loading}
+            disableConfirm={loading}
+        >
+            {error && <Alert variant='danger'>{error}</Alert>}
+            <AdminForm 
+                formData={formData}
+                formErrors={formErrors}
+                loading={loading}
+                onInputChange={handleInputChange}
+            />
+        </BaseModal>
+    );
+}
