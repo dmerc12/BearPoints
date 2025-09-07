93b751650cfe7688f13492cdb2ae39c6d345db53
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2025-09-07 01:40:14 -0500

    adds index for behavior type component exports

---
+++ client/src/components/behaviorTypes/index.ts
@@ -0,0 +1,5 @@
+export { default as BehaviorTypeTable } from './BehaviorTypeTable';
+export { CreateBehaviorTypeModal } from './CreateBehaviorTypeModal';
+export { EditBehaviorTypeModal } from './EditBehaviorTypeModal';
+export { DeleteBehaviorTypeModal } from './DeleteBehaviorTypeModal';
+export { BehaviorTypeForm } from './BehaviorTypeForm';
