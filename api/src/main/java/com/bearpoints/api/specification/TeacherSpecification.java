eec2a95b01b2d6b7cd29d6a043c11897f4c4c14d
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2026-02-17 13:20:47 -0600

    integrates addUserTextFilters to TeacherSpecification

--- api/src/main/java/com/bearpoints/api/specification/TeacherSpecification.java
+++ api/src/main/java/com/bearpoints/api/specification/TeacherSpecification.java
@@ -22,7 +22,7 @@ import java.util.List;
  *
  * @see TeacherSearchCriteria
  * @see Specification
- * @version 1.2
+ * @version 1.3
  * @author Dylan Mercer
  */
 @Component
@@ -38,18 +38,15 @@ public class TeacherSpecification {
         return (root, _, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
             predicates.add(criteriaBuilder.equal(root.get("user").get("role"), Role.TEACHER));
-            // Email filter
-            if (SpecificationUtils.isNotBlank(criteria.getEmail())) {
-                predicates.add(SpecificationUtils.likeIgnoreCase(root.get("user").get("email"), criteria.getEmail(), criteriaBuilder));
-            }
-            // First name filter
-            if (SpecificationUtils.isNotBlank(criteria.getFirstName())) {
-                predicates.add(SpecificationUtils.likeIgnoreCase(root.get("user").get("firstName"), criteria.getFirstName(), criteriaBuilder));
-            }
-            // Last name filter
-            if (SpecificationUtils.isNotBlank(criteria.getLastName())) {
-                predicates.add(SpecificationUtils.likeIgnoreCase(root.get("user").get("lastName"), criteria.getLastName(), criteriaBuilder));
-            }
+            // User text filters (email, first name, last name)
+            SpecificationUtils.addUserTextFilters(
+                    root.get("user"),
+                    criteria.getEmail(),
+                    criteria.getFirstName(),
+                    criteria.getLastName(),
+                    predicates,
+                    criteriaBuilder
+            );
             // Grade filter
             if (criteria.getGrade() != null) {
                 predicates.add(SpecificationUtils.equal(root.get("grade"), criteria.getGrade(), criteriaBuilder));
