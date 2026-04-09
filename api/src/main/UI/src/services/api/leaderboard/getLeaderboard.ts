2b596cd873bf5f0fc758738003134e976b67eb68
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2026-04-09 18:54:02 -0500

    update front end leaderboard api method

--- api/src/main/UI/src/services/api/leaderboard/getLeaderboard.ts
+++ api/src/main/UI/src/services/api/leaderboard/getLeaderboard.ts
@@ -1,11 +1,19 @@
-import { PaginatedLeaderboardEntries, Timeframe, fetchPaginated } from '../../index';
+import { Timeframe, GradeLevel, PagedResponseDTO, LeaderboardEntryDTO } from '../../types';
+import { withHealthAwareRetry } from "../withHealthAwareRetry.ts";
+import { api } from '../api';
 
-export const getLeaderboard = async (timeframe: Timeframe, page: number = 0,
-                                     size: number = 20, sortQuery?: string,
-                                     signal?: AbortSignal): Promise<PaginatedLeaderboardEntries> => {
-    let url = `api/leaderboard?timeframe=${timeframe}&page=${page}&size=${size}`;
-    if (sortQuery) {
-        url += `&${sortQuery}`;
-    }
-    return await fetchPaginated<PaginatedLeaderboardEntries>(url,'leaderboardEntries', signal);
+export const getLeaderboard = async (timeframe: Timeframe = Timeframe.WEEK,
+                                     teacherId?: number, grade?: GradeLevel, page: number = 0, size: number = 20,
+                                     sort?: string, signal?: AbortSignal)
+    : Promise<PagedResponseDTO<LeaderboardEntryDTO>> => {
+    const searchParams = new URLSearchParams();
+    searchParams.append('timeframe', timeframe);
+    if (teacherId !== undefined) searchParams.append('teacherId', String(teacherId));
+    if (grade !== undefined) searchParams.append('grade', grade);
+    searchParams.append('page', String(page));
+    searchParams.append('size', String(size));
+    if (sort) searchParams.append('sort', String(sort));
+    return withHealthAwareRetry(() =>
+        api.get<PagedResponseDTO<LeaderboardEntryDTO>>(`api/leaderboard?${searchParams}`, { signal })
+            .then(r => r.data));
 };
